ac9a07267fb10619de4a730abf4bcb00dc554f61
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-02 01:50:12 -0700

    home page navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,54 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/UofAlogo.png';
+import mascot from '../assets/Mascot.png';
 
 export default function Home() {
   return (
-    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-green-700">
+    <div className="w-screen min-h-screen flex flex-col items-center bg-green-700">
+            {/* Navbar */}
+            <nav className="w-full bg-green-800 text-yellow-300 flex items-center justify-between px-8 py-4">
+        {/* Logo */}
+        <div className="flex items-center">
+          <img src={logo} alt="UofA Logo" className="w-12 h-12 mr-4" />
+          <span className="font-bold text-xl">UofA Scheduler</span>
+        </div>
+
+        {/* Navbar Links */}
+        <ul className="flex space-x-6">
+          <li>
+            <Link
+              to="/home"
+              className="hover:text-yellow-500 transition duration-200"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="hover:text-yellow-500 transition duration-200"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="hover:text-yellow-500 transition duration-200"
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/help"
+              className="hover:text-yellow-500 transition duration-200"
+            >
+              Help
+            </Link>
+          </li>
+        </ul>
+      </nav>
       {/* Header Section */}
       <h1 className="font-semibold text-4xl text-yellow-500 mb-4">
         University of Alberta final exam scheduler
