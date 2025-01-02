a4725d7cdab6069d96e54f303064adbc864d2e2e
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-01 23:48:29 -0700

    bhai backend init

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+// Using express, Zod etc. to code a pretty cool backend
+const express = require('express');
+const { z } = require('zod');
+
+const app = express(); // Creating an express app
+
+console.log('Hello, world!');
+
+
+app.listen(5173); // Aligning with the port in the frontend
