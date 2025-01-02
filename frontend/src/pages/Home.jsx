190febe87979640b16163c215347ac47e541c0dc
saymanq <87442765+saymanq@users.noreply.github.com>
2025-01-01 23:56:53 -0700

    Courses Now display

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
 
 export default function Home() {
+  
+  
   return (
     <div>
-      <h1>Home Page</h1>
-      <Link to="/search">Go to Search</Link>
-      <Link to="/calendar">Go to Calendar</Link>
-      <Link to="/contact">Go to Contact</Link>
+      <h1 className="font-semibold text-2xl">Home Page</h1>
+
+      
     </div>
   )
 }
